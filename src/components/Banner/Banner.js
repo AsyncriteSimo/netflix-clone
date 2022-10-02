7fcebbf1feb3d7d76affd2974fad2b7fc826f14c
Simo <s0551187@htw-berlin.de>
2022-10-02 21:27:28 +0200

    fetching the data from tmdb and finalizing the banner okay

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './banner.css'
+import axios from '../../axios'
+import requests from "../../Request"
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals)
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random()*request.data.results.length - 1)
+                ]
+            )
+            return request;
+        }
+
+        fetchData()
+    }, [])
+
+    console.log(movie)
 
     function truncate(string, n){
         return string?.length > n ? string.substring(0, n-1) + '...' : string;
@@ -11,26 +31,17 @@ function Banner() {
     <header className="banner"
     style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
     }}> 
         <div className="banner__contents">
-            <h1 className="banner_title" >Movie Name</h1>
+            <h1 className="banner_title" >{movie?.title || movie?.name || movie?.original_name}</h1>
             <div className="banner__buttons">
                 <button className="banner__button" >Play</button>
                 <button className="banner__button" >My List</button>
             </div>
             <h1 className="banner__description">
-                {truncate(`Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, 
-            no sea takimata sanctus est Lorem ipsum 
-            dolor sit amet. Lorem ipsum dolor sit 
-            amet, consetetur sadipscing elitr, sed 
-            diam nonumy eirmod tempor invidunt ut labore 
-            et dolore magna aliquyam erat, sed diam voluptua.
-             At vero eos et accusam et justo duo dolores et 
-             ea rebum. Stet clita kasd gubergren, no sea 
-             takimata sanctus
-             est Lorem ipsum dolor sit amet.`, 150)}
+                {truncate(movie?.overview, 100)}
             </h1>
         </div>
 
