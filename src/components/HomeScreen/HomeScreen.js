2f22e28c05a068ec908db3d72d32cb88a01fb16f
Simo <s0551187@htw-berlin.de>
2022-10-03 12:32:18 +0200

    building film rows components. frontend done

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import requests from '../../Request'
 import Banner from '../Banner/Banner'
 import Nav from '../Nav/Nav'
+import Row from '../Row/Row'
 import './homeScreen.css'
 
 function Homescreen() {
@@ -13,6 +15,14 @@ function Homescreen() {
         <Banner />
 
         {/**Row */}
+        <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+        <Row title="Trending Now" fetchUrl={requests.fetchTopRated} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentary} />
     </div>
   )
 }
